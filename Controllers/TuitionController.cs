3f95465ae32e5d7c446668061d1d48358438053a
alexander l <exoxander15@gmail.com>
2023-02-21 17:22:15 -0700

    payment processing aprtiall works, need to add student balance to database, need to have that send to the stripe page.

--- Controllers/TuitionController.cs
+++ Controllers/TuitionController.cs
@@ -1,9 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Stripe;
+using Stripe.Checkout;
 
 namespace Assignment_1.Controllers
 {
     public class TuitionController : Controller
     {
+        public TuitionController()
+        {
+            StripeConfiguration.ApiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+        }
+
+        [HttpPost("create-checkout-session")]
+        public ActionResult CreateCheckoutSession()
+        {
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+        {
+          new SessionLineItemOptions
+          {
+            PriceData = new SessionLineItemPriceDataOptions
+            {
+              UnitAmount = 2000,
+              Currency = "usd",
+              ProductData = new SessionLineItemPriceDataProductDataOptions
+              {
+                Name = "Tuition_Payment",
+              },
+            },
+            Quantity = 1,
+          },
+        },
+                Mode = "payment",
+                SuccessUrl = "https://localhost:7099/Tuition/Success/",
+                CancelUrl = "https://localhost:7099/Tuition/Cancel",
+            };
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+
+        
+        [HttpGet("/Tuition/Success")]
+        public ActionResult OrderSuccess([FromQuery] string session_id)
+        {
+            return View();
+        }
+
+        public IActionResult Cancel()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
             return View();
