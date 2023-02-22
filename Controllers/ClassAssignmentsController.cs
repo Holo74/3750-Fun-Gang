23e65006ff6ad18d90f176a607a4dfb6a2d3a90b
Chase Powers <chasepowers@mail.weber.edu>
2023-02-21 20:10:18 -0700

    Pushing new changes

---
+++ Controllers/ClassAssignmentsController.cs
@@ -0,0 +1,30 @@
+﻿using Assignment_1.Data;
+using Assignment_1.Models;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+
+namespace Assignment_1.Controllers
+{
+    public class ClassAssignmentsController : Controller
+    {
+        private readonly Assignment_1Context _context;
+        public ClassAssignmentsController(Assignment_1Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index(int id)
+        {
+          
+            //var ClassID = HttpContext.Session.GetInt32("ClassID");
+           // var Assignments = from a in _context.ClassAssignments select a;
+            return View();
+        }
+        public IActionResult Create() 
+        {
+            var AssignmentID = HttpContext.Session.GetInt32("AssignmentID");
+            return View();
+        }
+    }
+}
