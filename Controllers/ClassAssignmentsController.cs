6779e912da5e46a88a748dab8ced232aab4f6f1d
alexander l <exoxander15@gmail.com>
2023-02-28 16:05:38 -0700

    task fails on submit to database primary key "Id" not being generated

--- Controllers/ClassAssignmentsController.cs
+++ Controllers/ClassAssignmentsController.cs
@@ -27,11 +27,50 @@ namespace Assignment_1.Controllers
             return View();
         }
 
-        public IActionResult Submit(int AssignmentID)
+        public IActionResult Assignment(int ID)
         {
+            AssignmentSubmissionViewModel ASVM = new AssignmentSubmissionViewModel();            
+
+            //set assignment according to ID
             var assignments = from a in _context.ClassAssignments select a;
-            ClassAssignments t = assignments.Where(x => x.Id == AssignmentID).FirstOrDefault();
-            return View(t);
+            ASVM.Assignment = assignments.Where(a => a.Id == ID).FirstOrDefault();
+
+            //check database for existing entry
+            int? UserID = HttpContext.Session.GetInt32("UserID");
+            int ClassID = ASVM.Assignment.ClassId;
+            HttpContext.Session.SetInt32("AssignmentID",ClassID);//just in case
+            int AssignmentID = ID;
+            var submission = from s in _context.AssignmentSubmissions select s;
+            if (UserID != null && ASVM.Assignment != null)
+            {
+                //find this users assignment for a class if it exists
+                ASVM.Submission = submission.Where(x => x.UserFK== UserID).Where(y => y.AssignmentFK == AssignmentID).Where(z => z.ClassFK == ClassID).FirstOrDefault();
+                
+            }
+            return View(ASVM);
+        }
+
+        public IActionResult Submit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Submit([Bind("Id,Data")] AssignmentSubmissions s)
+        {
+            //AssignmentSubmissions s = new AssignmentSubmissions();
+            int? UserID = HttpContext.Session.GetInt32("UserID");
+            int? AssignmentID = HttpContext.Session.GetInt32("AssignmentID");
+            s.UserFK = UserID == null ? 0 : UserID.Value;
+            s.AssignmentFK = AssignmentID == null ? 0 : AssignmentID.Value;
+            s.ClassFK = 7;
+            //s.Data= Data;
+            s.SubmitDate= DateTime.Now.Date;
+            //s.SubmitTime= default(DateTime).Add(DateTime.Now.TimeOfDay);
+
+			_context.AssignmentSubmissions.Add(s);
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("Assignment", new {ID = 7});//hardcode for testing
         }
     }
 }
---
+++ Models/AssignmentSubmissions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_1.Models
+{
+    public class AssignmentSubmissions
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserFK { get; set; }
+        [Required]
+        public int AssignmentFK { get; set; }
+        [Required]
+        public int ClassFK { get; set; }
+        public string? Data { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? SubmitDate { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime? SubmitTime { get; set; }
+    }
+}
